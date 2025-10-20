aef944470e7b1b1a777a68d42437b4720b313b21
slothMe-mlwr <moral.308698@ortigas-cainta.sti.edu.ph>
2025-10-20 22:19:43 +0800

    update B

--- controller/config.php
+++ controller/config.php
@@ -1,7 +1,7 @@
 <?php
 define("db_host", "localhost");
-define("db_user", "u185723031_revvedup");
-define("db_pass", "u185723031_revvedup@A");
+define("db_user", "root");
+define("db_pass", "hnyjnmrl0207.MYSQL");
 define("db_name", "u185723031_revvedupDB");
 
 
